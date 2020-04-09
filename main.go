a8c526406ada26ed61d26761a085116dc5dc1e2a
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-09 17:27:51 +0530

    Added Strctures for API

--- main.go
+++ main.go
@@ -6,7 +6,30 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
+
+type Source struct {
+	ID   interface{} `json:"id"`
+	Name string      `json:"name"`
+}
+
+type Article struct {
+	Source      Source    `json:"source"`
+	Author      string    `json:"author"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	URLToImage  string    `json:"urlToImage"`
+	PublishedAt time.Time `json:"publishedAt"`
+	Content     string    `json:"content"`
+}
+
+type Results struct {
+	Status       string    `json:"status"`
+	TotalResults int       `json:"totalResults"`
+	Articles     []Article `json:"articles"`
+}
 
 var tpl = template.Must(template.ParseFiles("index.html"))
 
